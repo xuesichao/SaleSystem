6091916d8042a494ae5fa32b950e270a0f216770
hmygrace <44683025+hmygrace@users.noreply.github.com>
2018-11-18 19:11:48 -0500

    Add files via upload

---
+++ database_project.sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 19, 2018 at 12:10 AM
+-- Server version: 5.7.21
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `business`
+--
+
+DROP TABLE IF EXISTS `business`;
+CREATE TABLE IF NOT EXISTS `business` (
+  `customer_id` int(10) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `company_income` int(20) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `customer_id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `kind` varchar(255) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home`
+--
+
+DROP TABLE IF EXISTS `home`;
+CREATE TABLE IF NOT EXISTS `home` (
+  `customer_id` int(10) NOT NULL,
+  `marry_status` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `age` int(3) NOT NULL,
+  `income` int(20) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+CREATE TABLE IF NOT EXISTS `inventory` (
+  `order_id` int(10) NOT NULL,
+  `order_date` datetime NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `quantity` int(20) NOT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `product_id` int(10) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `kind` varchar(255) NOT NULL,
+  `price` int(10) NOT NULL,
+  `cost` int(10) NOT NULL,
+  `amount` int(20) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `region`
+--
+
+DROP TABLE IF EXISTS `region`;
+CREATE TABLE IF NOT EXISTS `region` (
+  `region_id` int(10) NOT NULL,
+  `region_name` varchar(255) NOT NULL,
+  `region_manager` int(10) NOT NULL,
+  KEY `region_manager` (`region_manager`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salespersons`
+--
+
+DROP TABLE IF EXISTS `salespersons`;
+CREATE TABLE IF NOT EXISTS `salespersons` (
+  `salepersons_id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `job_title` varchar(255) NOT NULL,
+  `store_id` int(10) NOT NULL,
+  `salary` int(20) NOT NULL,
+  PRIMARY KEY (`salepersons_id`),
+  KEY `store_id` (`store_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+CREATE TABLE IF NOT EXISTS `store` (
+  `store_id` int(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `manager` int(10) NOT NULL,
+  `salesperson_num` int(10) NOT NULL,
+  `region_id` int(10) NOT NULL,
+  PRIMARY KEY (`store_id`),
+  KEY `manager` (`manager`),
+  KEY `region_id` (`region_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `order_id` int(10) NOT NULL,
+  `order_date` datetime DEFAULT NULL,
+  `product_id` int(10) DEFAULT NULL,
+  `salesperson_id` int(10) DEFAULT NULL,
+  `customer_id` int(10) DEFAULT NULL,
+  `quantity` int(10) DEFAULT NULL,
+  `store_id` int(10) DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `product_id` (`product_id`),
+  KEY `salesperson_id` (`salesperson_id`),
+  KEY `customer_id` (`customer_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
